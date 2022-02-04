35dc9c7c1207d924e2710c957f9d3a06db804d85
Johnson Osei Yeboah <johnyoat@gmail.com>
2022-02-04 03:24:30 -0600

    assets: added favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,56 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 import * as Icon from 'react-feather';
 import Link from "next/link";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>payFlow</title>
-        <meta name="description" content="Find what your employees earn" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>payFlow</title>
+                <meta name="description" content="Find what your employees earn"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="#">payFlow 1.0!</a>
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome to <a href="#">payFlow 1.0!</a>
+                </h1>
 
-        <p className={styles.description}>
-          Get started by uploading your CSV
-        </p>
+                <p className={styles.description}>
+                    Get started by uploading your CSV
+                </p>
 
-       <Link href={"/dashboard"}>
-           <div className={styles.grid}>
-               <div className={styles.card}>
-                   <div style={{
-                       display: 'flex',
-                   }}>
-                       <p>Click to upload  File</p>
-                       <Icon.File/>
-                   </div>
-               </div>
+                <Link href={"/dashboard"}>
+                    <div className={styles.grid}>
+                        <div className={styles.card}>
+                            <div style={{
+                                display: 'flex',
+                            }}>
+                                <p>Click to upload File</p>
+                                <Icon.File/>
+                            </div>
+                        </div>
 
-           </div>
-       </Link>
-      </main>
+                    </div>
+                </Link>
+            </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Copyright &#169; 2022
-        </a>
-      </footer>
-    </div>
-  )
+            <footer className={styles.footer}>
+                <a
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Copyright &#169; 2022
+                </a>
+            </footer>
+        </div>
+    )
 }
 
 export default Home
